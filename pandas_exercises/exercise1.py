715a858c735a9c4d7a7729e96bf69cafe79942fe
Natalya Langford <natalyaclangford@gmail.com>
2023-08-30 23:15:08 -0700

    data extraction

--- pandas_exercises/exercise1.py
+++ pandas_exercises/exercise1.py
@@ -6,11 +6,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# %matplotlib inline
-
+# initialize data for pandas 
 sales = pd.read_csv(
-    'data/sales_data.csv',
-    parse_dates=['Date']
-)
+'data/sales_data.csv',
+parse_dates=['Date'])
 
-sales.head()
+    
+def info():
+    print(sales.head()) # display the first few rows of a DataFrame
+    
+    print("Total Rows: {}  & Columns: {}".format(sales.shape[0], sales.shape[1]))
+    print()
+    print("Data Frame Info: ")
+    sales.info() # displays info about data frame 
+    
+    
+# call functions
+info()
